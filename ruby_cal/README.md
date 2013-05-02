b65b2b6476c6dca0f91cb4944941d68a4f5f3708
JonZach <Zach@yourgreenpal.com>
2013-05-02 09:51:42 -0500

    outputing correct date range for correct months. working on formating of string output.

--- ruby_cal/README.md
+++ ruby_cal/README.md
@@ -13,3 +13,26 @@
 <p> Step 2: Submit link to Integration Tests in Canvas.</p>
 <p> Step 3: Implement 1 method with unit tests.</p>
 <p> Step 4: Commit to Github and Submit link. </p>
+
+<h3>List of Methods Needed</h3>
+
+<p>month_header(month,year)</p>
+<p>days_of_week</p>
+<p>number_of_days(month,year)</p>
+    <ul>
+        <li>leap_year?</li>
+    </ul>
+<p>first_day_of_month(month,year)</p>
+    <ul>
+        <li>zellers congruence</li>
+    </ul>
+<p>leap_year?</p>
+<p>format_month(month,year)</p>
+    <ul>
+        <li>first_day_of_month</li>
+    </ul>
+<p>print_cal(month,year)</p>
+    <ul>
+        <li>first_day_of_month</li>
+        <li>format_month</li>
+    </ul>
--- ruby_cal/test/test_cal_integration.rb
+++ ruby_cal/test/test_cal_integration.rb
@@ -1,30 +1,30 @@
-# require 'test/unit'
-# require './myCalendar'
+require 'test/unit'
+require './myCalendar'
 
-# class CalIntegrationTests < Test::Unit::TestCase
+class CalIntegrationTests < Test::Unit::TestCase
 
-#     def test_final_integration
-#         assert_equal `cal 2 2013`, `ruby myCalendar.rb 2 2013`
-#     end
+    def test_final_integration
+        assert_equal `cal 2 2013`, `ruby myCalendar.rb 2 2013`
+    end
 
-#     def test_final_integration_leap_year
-#         assert_equal `cal 2 1996`, `ruby myCalendar.rb 2 1996`
-#     end
+    def test_final_integration_leap_year
+        assert_equal `cal 2 1996`, `ruby myCalendar.rb 2 1996`
+    end
 
-#     def test_final_integration_leap_century
-#         assert_equal `cal 2 2000`, `ruby myCalendar.rb 2 2000`
-#     end
+    def test_final_integration_leap_century
+        assert_equal `cal 2 2000`, `ruby myCalendar.rb 2 2000`
+    end
 
-#     def test_final_integration_leap_year_not_leap_century
-#         assert_equal `cal 2 1900`, `ruby myCalendar.rb 2 1900`
-#     end
+    def test_final_integration_leap_year_not_leap_century
+        assert_equal `cal 2 1900`, `ruby myCalendar.rb 2 1900`
+    end
 
-#     def test_final_integration_more_than_usual_weeks
-#         assert_equal `cal 3 2013`, `ruby myCalendar.rb 3 2013`
-#     end
+    def test_final_integration_more_than_usual_weeks
+        assert_equal `cal 3 2013`, `ruby myCalendar.rb 3 2013`
+    end
 
-#     def test_final_integration_less_than_usual_weeks
-#         assert_equal `cal 2 2009`, `ruby myCalendar.rb 2 2009`
-#     end
+    def test_final_integration_less_than_usual_weeks
+        assert_equal `cal 2 2009`, `ruby myCalendar.rb 2 2009`
+    end
 
-# end
+end
